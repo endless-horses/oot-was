7f519508de4db24e57fe338f96bba57529ee2f46
Subi Hwang <ghkdtnql@gmail.com>
2024-01-08 14:36:11 +0900

    chore: spring 프로젝트 생성

---
+++ src/main/java/com/endlesshorses/oot/OotApplication.java
@@ -0,0 +1,13 @@
+package com.endlesshorses.oot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endlesshorses/oot/OotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endlesshorses.oot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
