e58bb8e3ecab8c23fb2797d64fdfedd168e141e1
Subi Hwang <ghkdtnql@gmail.com>
2024-01-14 17:15:45 +0900

    fix: SecurityConfig build.gradle 파일 수정#9

--- src/main/java/com/endlesshorses/oot/swagger/SwaggerConfig.java
+++ src/main/java/com/endlesshorses/oot/swagger/SwaggerConfig.java
@@ -3,46 +3,22 @@ package com.endlesshorses.oot.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
 	@Bean
-	public GroupedOpenApi getItemApi() {
-
-		return GroupedOpenApi
-				.builder()
-				.group("item")
-				.pathsToMatch("/api/item/**")
-				.build();
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(apiInfo());
 	}
 
-	@Bean
-	public GroupedOpenApi getMemberApi() {
-
-		return GroupedOpenApi
-				.builder()
-				.group("member")
-				.pathsToMatch("/api/member/**")
-				.build();
-
-	}
-
-	@Bean
-	public OpenAPI getOpenApi() {
-
-		return new OpenAPI().components(new Components())
-				.info(getInfo());
-
-	}
-
-	private Info getInfo() {
+	private Info apiInfo() {
 		return new Info()
-				.version("1.0.0")
-				.description("COMMERCE REST API DOC")
-				.title("COMMERCE");
+				.title("OOT")
+				.description("OOT API 명세")
+				.version("1.0.0");
 	}
 }
