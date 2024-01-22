b8385422687a210fbc653e2e1d1235b9f0709e66
byeon22 <minjeongs0202@gmail.com>
2024-01-22 17:29:26 +0900

    feat: wheel 목록 조회 api - dto 구현

---
+++ src/main/java/com/endlesshorses/oot/custom/wheel/dto/WheelListResponseDto.java
@@ -0,0 +1,24 @@
+package com.endlesshorses.oot.custom.wheel.dto;
+
+import com.endlesshorses.oot.custom.wheel.entity.Wheel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class WheelListResponseDto {
+
+    private String name;
+    private Long price;
+    private String imageUrl;
+    private String explanation;
+
+    @Builder
+    public WheelListResponseDto(Wheel wheel) {
+        this.name = wheel.getName();
+        this.price = wheel.getPrice();
+        this.imageUrl = wheel.getImageUrl();
+        this.explanation = wheel.getExplanation();
+    }
+}
