2aa7016b26373460d72cc4729dcd9d21f4f87e01
byeon22 <minjeongs0202@gmail.com>
2024-01-24 01:28:32 +0900

    fix: RequestMapping 추가, list()로 메서드명 변경

--- src/main/java/com/endlesshorses/oot/custom/wheel/controller/WheelController.java
+++ src/main/java/com/endlesshorses/oot/custom/wheel/controller/WheelController.java
@@ -7,7 +7,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,20 +17,28 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Wheel", description = "휠 모양 관련 API")
+@RequestMapping("/wheels")
 public class WheelController {
     private final WheelService wheelService;
 
-    @GetMapping("/wheel")
+    @GetMapping
+    @Operation(summary = "Wheel 목록 조회 메서드", description = "클라이언트가 요청한 휠 모양 목록 정보를 조회하기 위한 메서드")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "휠 모양 목록 조회 성공"),
+    })
+    public ResponseEntity<?> list() {
+        List<WheelListResponseDto> wheelList = wheelService.list();
+
+        return ResponseEntity.ok().body(wheelList);
+    }
+
+    /*
+    @GetMapping
     @Operation(summary = "Wheel 목록 조회 메서드", description = "클라이언트가 요청한 휠 모양 목록 정보를 조회하기 위한 메서드")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "휠 모양 목록 조회 성공"),
             @ApiResponse(responseCode = "400(403)", description = "휠 모양 목록 조회 실패"),
     })
-    public List<WheelListResponseDto> findAll() {
-        return wheelService.list();
-    }
-
-    /*
     public ResponseEntity<Message> listWheel(HttpServletRequest request) {
         WheelResponseDto wheelResponseDto = (WheelResponseDto) wheelService.list();
 
