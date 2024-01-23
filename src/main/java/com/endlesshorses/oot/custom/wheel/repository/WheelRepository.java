3beb67999a518392ab1f2c3192be5398a61f02e0
byeon22 <minjeongs0202@gmail.com>
2024-01-24 01:27:01 +0900

    fix: findAll() 오버라이딩 삭제

--- src/main/java/com/endlesshorses/oot/custom/wheel/repository/WheelRepository.java
+++ src/main/java/com/endlesshorses/oot/custom/wheel/repository/WheelRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface WheelRepository extends JpaRepository<Wheel, Long> {
-    @Override
-    List<Wheel> findAll();
 }
