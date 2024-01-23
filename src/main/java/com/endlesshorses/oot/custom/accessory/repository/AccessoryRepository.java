4902202fda3ae90d1d36b0149f1a143fef4c21ae
byeon22 <minjeongs0202@gmail.com>
2024-01-24 01:51:36 +0900

    feat: 액세서리 repository 작성

---
+++ src/main/java/com/endlesshorses/oot/custom/accessory/repository/AccessoryRepository.java
@@ -0,0 +1,8 @@
+package com.endlesshorses.oot.custom.accessory.repository;
+
+import com.endlesshorses.oot.custom.accessory.enity.Accessory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccessoryRepository extends JpaRepository<Accessory, Long> {
+
+}
