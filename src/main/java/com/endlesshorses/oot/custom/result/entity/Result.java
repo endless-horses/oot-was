9aedad1df72df21593285a4567d1f37c98eea678
Subi Hwang <ghkdtnql@gmail.com>
2024-02-20 16:42:32 +0900

    feat: result entity 생성 #60

--- src/main/java/com/endlesshorses/oot/custom/result/entity/Result.java
+++ src/main/java/com/endlesshorses/oot/custom/result/entity/Result.java
@@ -1,5 +1,6 @@
 package com.endlesshorses.oot.custom.result.entity;
 
+import com.endlesshorses.oot.custom.accessory.enity.Accessory;
 import com.endlesshorses.oot.custom.color.entity.Color;
 import com.endlesshorses.oot.custom.font.entity.Font;
 import com.endlesshorses.oot.custom.pattern.entity.Pattern;
@@ -7,18 +8,20 @@ import com.endlesshorses.oot.custom.wheel.entity.Wheel;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.KeyFactory;
 import org.springframework.data.annotation.CreatedDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class Result {
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@GeneratedValue(generator = "uuid2")
+	@Column(columnDefinition = "BINARY(16)")
+	private UUID id;
 
 	@ManyToOne
 	@JoinColumn(name = "PATTERN_ID", nullable = false)
@@ -36,10 +39,12 @@ public class Result {
 	@JoinColumn(name = "FONT_COLOR_ID", nullable = false)
 	private Color color;
 
+	@ManyToOne
+	@JoinColumn(name = "ACCESSORY_ID")
+	private Accessory accessory;
 
 	@Column(nullable = false)
 	@CreatedDate
 	private LocalDateTime createdAt;
-
 
 }
